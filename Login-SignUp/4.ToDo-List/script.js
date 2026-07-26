33e3fb0b243f6dacd7a45bfdac0031fa8274979a
ali <ali.yousefi7518@gmail.com>
2026-07-26 18:32:27 +0430

    Todo-list(add create-list func)

--- Login-SignUp/4.ToDo-List/script.js
+++ Login-SignUp/4.ToDo-List/script.js
@@ -1,7 +1,8 @@
 let list = document.querySelector("main");
-let addInput = document.querySelector('input.add');
+let addInput = document.querySelector('.add');
 let searchInput = document.querySelector('input.search');
-let addBtn = document.querySelector('#add-btn');;
+let addBtn = document.querySelector("#addbtn");
+
 
 list.addEventListener("click",(e)=>{
     if(e.target.nodeName =='BUTTON' && e.target.className =='delete'){
@@ -11,9 +12,36 @@ list.addEventListener("click",(e)=>{
             listEmptyMsg.style.textAlign = 'center'
             listEmptyMsg.style.color = '#333'
             listEmptyMsg.innerText = 'your list is empty'
+            listEmptyMsg.id = 'emptyMsg'
             list.appendChild(listEmptyMsg);
         }
     }
 })
 
 
+addBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(!addInput.value){
+        return
+    }
+    if(document.querySelector('#emptyMsg')){
+        document.querySelector('#emptyMsg').remove()
+    }
+    list.append(createListItem(addInput.value))
+    addInput.value =''
+})
+
+
+function createListItem(itemValue){
+    let item = document.createElement('div')
+    let title = document.createElement('p')
+    let button = document.createElement('button')
+    item.className = 'data'
+    title.innerText = itemValue
+    button.className = 'delete'
+    button.innerText = 'delete'
+    item.appendChild(title)
+    item.appendChild(button)
+    return item
+
+}
