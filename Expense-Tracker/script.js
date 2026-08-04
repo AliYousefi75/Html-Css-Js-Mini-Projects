7851e9644b8364e9ad30b3032b757fc98a263121
ali <ali.yousefi7518@gmail.com>
2026-08-04 21:53:00 +0430

    Expense-Tracker(add-transaction func)

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -6,4 +6,33 @@ let transactionFormEl = document.getElementById('transaction-form');
 let descriptionEl = document.getElementById('description');
 let amountEl =document.getElementById('amount');
 
+//tabdile reshteh be array
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+transactionFormEl.addEventListener('submit',addTransaction);
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    // get form values
+    const description = descriptionEl.value.trim();
+    //tabdile reshte be adade ashari
+    const amount = parseFloat(amountEl.value);
+
+    transactions.push({
+        id:Date.now(),
+        description = description,
+        amount = amount
+    });
+
+    //array ra be soorate yek reshteh json zakhire mikonad
+    localStorage.setItem('transactions',JSON.stringify(transactions));
+
+    updateTransactionList();
+    updateSummary();
+
+    //tamame voroodihaye form ra khali mikonad 
+    transactionFormEl.reset();
+}
+
 
