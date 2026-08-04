6e10cd3ad7db2f866188d94f4643644343a748f4
ali <ali.yousefi7518@gmail.com>
2026-08-04 21:19:16 +0430

    Expense-Tracker(add base script)

---
+++ Expense-Tracker/script.js
@@ -0,0 +1,9 @@
+let balanceEl = document.getElementById('balance');
+let incomeAmountEl = document.getElementById('income-amount');
+let expenseAmountEl = document.getElementById('expense-amount');
+let transactionListEl = document.getElementById('transaction-list');
+let transactionFormEl = document.getElementById('transaction-form');
+let descriptionEl = document.getElementById('description');
+let amountEl =document.getElementById('amount');
+
+
