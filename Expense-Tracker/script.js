c930be8fe8754cd1ee9c988966c9ba128788c9cd
ali <ali.yousefi7518@gmail.com>
2026-08-04 22:08:52 +0430

    Expense-Tracker(add create-transaction-element)

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -45,3 +45,19 @@ function updateTransactionList(){
         transactionListEl.appendChild(transactionEl)
     });
 }
+
+function createTransactionElement(transaction){
+    const li = document.createElement('li');
+    li.classList.add('transaction');
+    li.classList.add(transaction.amount > 0 ? 'income' : 'expense')
+
+    li.innerHTML = `
+    <span>${transaction.description}</span>
+
+    <span>${transaction.amount}
+        <button class='delete-btn' onclick = 'removeTransaction(${transaction.id})'>x</button>
+    </span>
+    `;
+
+    return li;
+}
