f4d2d61f3af2d24791f91b16423b9495af0a51c8
ali <ali.yousefi7518@gmail.com>
2026-08-17 21:44:54 +0430

    Bookmark-Saver(add-bookmark-btn listener)

--- Login-SignUp/6.Bookmark-Saver/script.js
+++ Login-SignUp/6.Bookmark-Saver/script.js
@@ -5,3 +5,23 @@ const bookmarkUrlInput = document.getElementById('bookmark-url');
 
 document.addEventListener("DOMContentLoaded", loadBookmarks);
 
+addBookmarkBtn.addEventListener('click',function(){
+    const name = bookmarkNameInput.value.trim();
+    const url = bookmarkUrlInput.value.trim();
+
+    if(!name || !url){
+        alert('please enter both name and url');
+        return;
+    }else{
+        if(!url.startWith('http://') && !url.startWith('https://')){
+            alert('please enter a valid url starting with http:// or https://');
+            return;
+        }
+
+        addBookmark(name,url);
+        saveBookmark(name,url);
+        bookmarkNameInput.value ='';
+        bookmarkUrlInput.value = '';
+    }
+});
+
