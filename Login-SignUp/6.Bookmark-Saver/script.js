68e55caf1b24f61bdb73462ebc3f118970daed6b
ali <ali.yousefi7518@gmail.com>
2026-08-17 21:37:05 +0430

    Bookmark-Saver(add base script)

---
+++ Login-SignUp/6.Bookmark-Saver/script.js
@@ -0,0 +1,7 @@
+const addBookmarkBtn = document.getElementById('add-bookmark');
+const bookmarkList = document.getElementById('bookmark-list');
+const bookmarkNameInput = document.getElementById('bookmark-name');
+const bookmarkUrlInput = document.getElementById('bookmark-url');
+
+document.addEventListener("DOMContentLoaded", loadBookmarks);
+
