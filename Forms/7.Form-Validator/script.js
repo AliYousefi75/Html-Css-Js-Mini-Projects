c8f34188055a3cca70c258148b24178b90bf8a32
ali <ali.yousefi7518@gmail.com>
2026-09-16 22:54:03 +0430

    Form-Validator(add check-password-match)

--- Forms/7.Form-Validator/script.js
+++ Forms/7.Form-Validator/script.js
@@ -30,3 +30,12 @@ form.addEventListener("submit", function (e) {
     });
   }
 });
+
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+    return false;
+  }
+  return true;
+}
